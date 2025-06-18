9661153407816350883290efd94857dd5ce98b92
Stella-055 <stellaongayo243@gmail.com>
2025-06-18 18:58:10 +0300

    discount styling

--- src/cpmponents/Foods.jsx
+++ src/cpmponents/Foods.jsx
@@ -1,41 +1,47 @@
-import React from 'react'
-import { foodlist } from '../../public/foodlist'
+import React from "react";
+import { foodlist } from "../../public/foodlist";
+import { FaStar } from "react-icons/fa6";
 
 const Foods = () => {
   return (
     <>
-   {
-    foodlist.map(( food,id)=>{
-  return (
-    <div key={id} className='food-container'>
-      <div className='food-container-image-div'>
-        <img src={food.image} alt="imaga"  />
-      </div>
-      <div className='food-container-details'>
-      <h3>{food.title}</h3>
-      <p>{food.description}</p>
-      <h3 className='food-container-details-price'>${food.price}</h3>
-      </div>
-    </div>
-  )
-
-    })
-   }
-      
+      {foodlist.map((food, id) => {
+        return (
+          <div key={id} className="food-container">
+            <div className="food-container-image-div">
+              <img src={food.image} alt="imaga" />
+              {food.discount && (
+                <div className="food-container-discount">
+                  <span>{food.discount}%</span>
+                </div>
+              )}
+              {food.rating && (
+                <div className="food-container-rating">
+                  {" "}
+                  <span>
+                    <FaStar className="star" /> {food.rating}
+                  </span>
+                </div>
+              )}
+            </div>
+            <div className="food-container-details">
+              <h3>{food.title}</h3>
+              <p>{food.description}</p>
+              <h3 className="food-container-details-price">${food.price}</h3>
+            </div>
+          </div>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
 const Foodlist = () => {
   return (
-  <div className='foodcontainer-wrapper'>
-    <Foods/>
-  </div>
-   
-  )
-}
+    <div className="foodcontainer-wrapper">
+      <Foods />
+    </div>
+  );
+};
 
-
-
-
-export default Foodlist
+export default Foodlist;
