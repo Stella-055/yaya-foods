dfff5acaebb6d966002fa23d2107723b559fbe49
Stella-055 <stellaongayo243@gmail.com>
2025-06-18 20:13:08 +0300

    used prettier

--- src/cpmponents/Header.jsx
+++ src/cpmponents/Header.jsx
@@ -1,55 +1,54 @@
-import React from 'react'
+import React from "react";
 import { MdFoodBank } from "react-icons/md";
 import { RiDrinksLine } from "react-icons/ri";
 import { CiPizza } from "react-icons/ci";
 import { LuSalad } from "react-icons/lu";
-import { GiHamburgerMenu } from "react-icons/gi"
+import { GiHamburgerMenu } from "react-icons/gi";
 import { GiHotSpices } from "react-icons/gi";
 import { LuCakeSlice } from "react-icons/lu";
-const NavLinks= [
-{id:"1",
-    icon:<MdFoodBank/>,
-    name:"All"
-},
-{id:"2",
-    icon:<RiDrinksLine/>,
-    name:"Drinks"
-},
-{
-    id:"3",
-    icon:<CiPizza/>,
-    name:"Pizza"
-},
-{id:"4",
-    icon:<LuSalad/>,
-    name:"Salad"
-},
-{
-    id:"5",
-    icon:<GiHotSpices/>,
-    name:"Spicy"
-},
-{id:"6",
-    icon:<LuCakeSlice/>,
-    name:"Sweets"
-}
-
-]
+const NavLinks = [
+  { id: "1", icon: <MdFoodBank />, name: "All" },
+  { id: "2", icon: <RiDrinksLine />, name: "Drinks" },
+  {
+    id: "3",
+    icon: <CiPizza />,
+    name: "Pizza",
+  },
+  { id: "4", icon: <LuSalad />, name: "Salad" },
+  {
+    id: "5",
+    icon: <GiHotSpices />,
+    name: "Spicy",
+  },
+  { id: "6", icon: <LuCakeSlice />, name: "Sweets" },
+];
 const Header = () => {
   return (
     <header>
-        <GiHamburgerMenu className='menuicon'/>
-   <div>
- {NavLinks.map((link,id)=>{
- return( <div className= { link.id == 1 ? "navcomponent-active":"navcomponent"} key={id}>
-  {link.icon}
-   <a className= {`navcomponent-link ${link.id ==1? "navcomponent-link-active":""}` }href="">{link.name}</a> </div>
- )
-})}
-   </div>
-   <button>View All</button>
+      <GiHamburgerMenu className="menuicon" />
+      <div>
+        {NavLinks.map((link, id) => {
+          return (
+            <div
+              className={link.id == 1 ? "navcomponent-active" : "navcomponent"}
+              key={id}
+            >
+              {link.icon}
+              <a
+                className={`navcomponent-link ${
+                  link.id == 1 ? "navcomponent-link-active" : ""
+                }`}
+                href=""
+              >
+                {link.name}
+              </a>{" "}
+            </div>
+          );
+        })}
+      </div>
+      <button>View All</button>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
