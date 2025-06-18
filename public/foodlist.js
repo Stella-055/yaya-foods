4c976156454306d50e2f956ace4862ab22199c6a
Stella-055 <stellaongayo243@gmail.com>
2025-06-18 19:51:43 +0300

    added more dishes in the array list

--- public/foodlist.js
+++ public/foodlist.js
@@ -1,25 +1,104 @@
- export const foodlist=[
+export const foodlist = [
+  {
+    id: "1",
+    image: "spaggeti.jpeg",
+    description:
+      "pasta dish made with fatty cured pork,hard cheese eggs and salt",
+    title: "Spaghetti Carbonara",
+    price: "30.00",
+    discount: "20",
+    rating: "5.00",
+  },
 
-{
-    id:"1",
-    image:"spaggeti.jpeg",
-    description:"pasta dish made with fatty cured pork,hard cheese eggs and salt",
-    title:"Spaghetti Carbonara",
-    price:"30.00",
-    discount:"20",
-    rating:"5.00"
-},
+  {
+    id: "2",
+    image: "chickenandrice.jpeg",
+    description:
+      "This one-pot chicken and rice recipe is quick enough for a weeknight but harnesses long-simmered flavor",
+    title: "Chicken and Rice",
+    price: "28.00",
+    discount: "10",
+    rating: "5.00",
+  },
+  {
+    id: "3",
+    image: "turkeymeatloaf.webp",
+    description:
+      "Pasta dish made with fatty cured pork,hard cheese eggs and salt",
+    title: "Turkey Meatloaf",
+    price: "40.00",
+    discount: "10",
+    rating: "4.00",
+  },
+  {
+    id: "4",
+    image: "1700604212111.jpeg",
+    description:
+      "Pasta dish made with fatty cured pork,hard cheese eggs and salt",
+    title: "Turkey Meatloaf",
+    price: "40.00",
+    discount: "10",
+    rating: "4.00",
+  },
+  {
+    id: "5",
+    image: "bakedsalmon.jpeg",
+    description:
+      "Pasta dish made with fatty cured pork,hard cheese eggs and salt",
+    title: "Baked salmon",
+    price: "40.00",
 
-{
-    id:"2",
-    image:"spaggeti.jpeg",
-    description:"pasta dish made with fatty cured pork,hard cheese eggs and salt",
-    title:"Spaghetti Carbonara",
-    price:"30.00",
-    discount:"20",
-    rating:"5.00"
-}
+    rating: "5.00",
+  },
 
- ]
+  {
+    id: "6",
+    image: "garlicroastedpotatoes.jpeg",
+    description:
+      "Pasta dish made with fatty cured pork,hard cheese eggs and salt",
+    title: "Garlic Roasted Potatoes",
+    price: "40.00",
+    discount: "10",
+    rating: "4.00",
+  },
+  {
+    id: "7",
+    image: "fries.jpg",
+    description:
+      "Pasta dish made with fatty cured pork,hard cheese eggs and salt",
+    title: "Finesse Fries",
+    price: "20.00",
+    discount: "5",
+    rating: "5.00",
+  },
+  {
+    id: "8",
+    image: "carrotcake.jpeg",
+    description:
+      "Pasta dish made with fatty cured pork,hard cheese eggs and salt",
+    title: "Carrot Cake",
+    price: "30.00",
+    discount: "15",
+    rating: "4.00",
+  },
+  {
+    id: "9",
+    image: "dirtybloodymary.jpeg",
+    description:
+      "Pasta dish made with fatty cured pork,hard cheese eggs and salt",
+    title: "Dirty Bloody Mary",
+    price: "10.00",
 
+    rating: "5.00",
+  },
+  {
+    id: "10",
+    image: "beerbatter.webp",
+    description:
+      "Pasta dish made with fatty cured pork,hard cheese eggs and salt",
+    title: "Beer Batter",
+    price: "40.00",
 
+    discount: "15",
+  },
+];
